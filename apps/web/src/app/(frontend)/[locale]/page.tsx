660225723c178e3a2c243a17ae796e16d608d5dc
Alex <truonggiang190689@gmail.com>
2026-01-19 19:01:36 +0700

    Update dependencies in bun.lock and package.json; add @workspace/ui for shared UI components. Refactor PostCSS configuration to utilize shared styles and remove unused global CSS. Introduce new layout and Not Found page components for improved frontend structure and user experience.

--- apps/web/src/app/(frontend)/[locale]/page.tsx
+++ apps/web/src/app/(frontend)/[locale]/page.tsx
@@ -1,12 +1,19 @@
+import { Spinner } from "@workspace/ui/components/spinner";
 import { useTranslations } from "next-intl";
+import { Button } from "@/components/ui/button";
 import { Link } from "@/i18n/navigation";
-
 export default function Page() {
   const t = useTranslations("HomePage");
   return (
     <div className="flex flex-col gap-4">
       {t("title")}
       <Link href="/test">test page</Link>
+      <div>
+        <Button className="gap-2" disabled size={"sm"}>
+          <Spinner className="size-4" />
+          Loading...
+        </Button>
+      </div>
     </div>
   );
 }
